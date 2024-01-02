dfda3b70e0d55c404f1d247545b172ca921bdc0e
mmickert <mickert@student-42heilbronn.de>
2024-01-02 16:13:03 +0100

    next steps

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: mickert <mickert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 13:07:18 by mickert           #+#    #+#             */
-/*   Updated: 2024/01/02 14:15:17 by mickert          ###   ########.fr       */
+/*   Updated: 2024/01/02 15:56:09 by mickert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ typedef struct
 {
 	char **map;
 	int	fd;
+	int	line_length;
+	int	line_counter;
 
 }	t_so_long;
 
@@ -31,7 +33,7 @@ int		main(int argc, char **argv);
 void	parse(char **argv, t_so_long *game);
 int		check_map_shape(t_so_long *game);
 int		fill_map(t_so_long *game);
-void	allocate_map(t_so_long *game, char **argv, int line_length);
+int		allocate_map(t_so_long *game, char **argv);
 int		check_map_components(t_so_long *game);
 
 
--- suorce/so_long.c
+++ suorce/so_long.c
@@ -6,7 +6,7 @@
 /*   By: mickert <mickert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 13:07:02 by mickert           #+#    #+#             */
-/*   Updated: 2024/01/02 14:51:13 by mickert          ###   ########.fr       */
+/*   Updated: 2024/01/02 16:12:48 by mickert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int argc, char **argv)
 	ft_memset(&game, 0, sizeof(t_so_long));
 	if (argc != 2)
 	{
-		ft_putstr_fd("Invalid input. Too many or few arguments.\n", 2);
+		ft_putstr_fd("Error\nInvalid input. Too many or few arguments.\n", 2);
 		exit(EXIT_FAILURE);
 	}
 	parse(argv, &game);
@@ -37,23 +37,24 @@ void	parse(char **argv, t_so_long *game)
 		i++;
 	if (ft_strnstr(argv[1], ".ber", i) == NULL)
 	{
-		ft_putstr_fd("Input needs to be a .ber file.\n", 2);
+		ft_putstr_fd("Error\nInput needs to be a .ber file.\n", 2);
 		exit(EXIT_FAILURE);
 	}
 	game->fd = open(argv[1], O_RDONLY);
 	if (game->fd == -1)
 	{
-		ft_putstr_fd("Invalid file path.'\n", 2);
+		ft_putstr_fd("Error\nInvalid file path.'\n", 2);
 		exit(EXIT_FAILURE);
 	}
-	i = check_map_shape(game);
-	allocate_map(game, argv, i);
+	check_map_shape(game);
+	game->line_counter = allocate_map(game, argv);
 	check_map_components(game);
 }
 
 int	is_allowed_char(char c)
 {
-	return (c == '1' || c == '0' || c == 'C' || c == 'P' || c == 'E' || (c >= 9 && c <= 13) || c == 32);
+	return (c == '1' || c == '0' || c == 'C' || c == 'P' || c == 'E' || (c >= 9
+			&& c <= 13) || c == 32);
 }
 
 int	check_map_components(t_so_long *game)
@@ -69,31 +70,34 @@ int	check_map_components(t_so_long *game)
 		{
 			if (!is_allowed_char(game->map[i][j]))
 			{
-				ft_printf("Invalid character found in the map.\n");
+				ft_printf("Error\nInvalid character found in the map.\n");
 				exit(1);
 			}
 			j++;
 		}
 		i++;
 	}
+ft_printf("linelength: %d\n", game->line_length);
+ft_printf("linecount: %d\n", game->line_counter);
 	return (0);
 }
 
-void	allocate_map(t_so_long *game, char **argv, int line_length)
+int	allocate_map(t_so_long *game, char **argv)
 {
 	game->fd = open(argv[1], O_RDONLY);
 	if (game->fd == -1)
 	{
-		ft_putstr_fd("Invalid file path.\n", 2);
+		ft_putstr_fd("Error\nInvalid file path.\n", 2);
 		exit(EXIT_FAILURE);
 	}
-	game->map = (char **)ft_calloc(line_length, sizeof(char *));
+	game->map = (char **)ft_calloc(game->line_length, sizeof(char *));
 	if (game->map == NULL)
 	{
-		ft_putstr_fd("Memory allocation error.\n", 2);
+		ft_putstr_fd("Error\nMemory allocation error.\n", 2);
 		exit(EXIT_FAILURE);
 	}
-	fill_map(game);
+	game->line_counter = fill_map(game);
+	return (game->line_counter);
 }
 
 int	fill_map(t_so_long *game)
@@ -101,34 +105,35 @@ int	fill_map(t_so_long *game)
 	char	*line;
 	int		i;
 
+	game->line_counter = 0;
 	i = 0;
 	while (1)
 	{
 		line = get_next_line(game->fd);
 		if (line == NULL)
 			break ;
+		game->line_counter++;
 		game->map[i] = ft_strdup(line);
 		free(line);
 		if (game->map[i] == NULL)
 		{
-			ft_putstr_fd("Memory allocation error.\n", 2);
+			ft_putstr_fd("Error\nMemory allocation error.\n", 2);
 			exit(EXIT_FAILURE);
 		}
 		i++;
 	}
-	return (0);
+	return (game->line_counter);
 }
 
 int	check_map_shape(t_so_long *game)
 {
 	int		i;
-	int		y;
 	char	*line;
 
 	i = 0;
 	while (1)
 	{
-		y = i;
+		game->line_length = i;
 		line = get_next_line(game->fd);
 		if (line == NULL)
 			break ;
@@ -137,11 +142,20 @@ int	check_map_shape(t_so_long *game)
 			i++;
 		if (i > 0 && line[i - 1] == '\n')
 			i--;
-		if (y == 0)
-			y = i;
+		if (game->line_length == 0)
+			game->line_length = i;
 		free(line);
-		if (y != i && (ft_putstr_fd("Map is not rectangular.\n", 2), 1))
+		if (game->line_length != i
+			&& (ft_putstr_fd("Error\nMap is not rectangular.\n", 2), 1))
 			exit(1);
 	}
-	return (close(game->fd), y);
+	return (close(game->fd), game->line_length);
 }
+
+/*next steps:
+- are the walls all 1s? (using line count and length)
+- are there only one E and P?
+- how many Cs?
+- floodfill - erf - can p reach e?
+- are all the Cs reachable? (how many cs can p reach, is that the same as c number?)
+*/
